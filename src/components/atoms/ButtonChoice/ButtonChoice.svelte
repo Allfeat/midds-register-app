56139d5296bff3b7aa28b56c6c30ec0f5bf8722d
SailorSnoW <sailorsnow@pm.me>
2025-01-29 17:01:02 +0100

    regroup midds entity with categories

--- src/components/atoms/ButtonChoice/ButtonChoice.svelte
+++ src/components/atoms/ButtonChoice/ButtonChoice.svelte
@@ -1,64 +1,58 @@
 <script lang="ts">
-    import type { Snippet } from 'svelte'
+  import type { Snippet } from "svelte";
 
-    let {
-        active = false,
-        disabled = false,
-        children,
-        onclick,
-    }: {
-        active?: boolean
-        disabled?: boolean
-        children: Snippet
-        onclick?: () => any
-    } = $props()
+  let {
+    active = false,
+    disabled = false,
+    children,
+    onclick,
+  }: {
+    active?: boolean;
+    disabled?: boolean;
+    children: Snippet;
+    onclick?: () => any;
+  } = $props();
 </script>
 
-<button
-    class="button-choice"
-    class:is-active={active}
-    {onclick}
-    {disabled}
->
-    {@render children()}
+<button class="button-choice" class:is-active={active} {onclick} {disabled}>
+  {@render children()}
 </button>
 
 <style lang="scss">
-    button {
-        display: flex;
-        align-items: center;
-        gap: 12px;
-        height: 88px;
-        padding: 0 40px;
-        background: var(--color-light-10);
-        font-family: inherit;
-        font-weight: 700;
-        font-size: pxrem(32px);
-        line-height: 1;
-        color: var(--color-light);
-        border: none;
-        border-radius: 100vh;
-        cursor: pointer;
-        transition-property: color, background-color;
-        transition-duration: 0.5s;
-        transition-timing-function: var(--ease-quart);
+  button {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    padding: 20px;
+    background: var(--color-light-10);
+    font-family: inherit;
+    font-weight: 700;
+    font-size: pxrem(24px);
+    line-height: 1;
+    color: var(--color-light);
+    border: none;
+    border-radius: 100vh;
+    cursor: pointer;
+    transition-property: color, background-color;
+    transition-duration: 0.5s;
+    transition-timing-function: var(--ease-quart);
 
-        // State: Hover
-        &:hover {
-            background-color: var(--color-light-20);
-        }
-
-        // State: Active
-        &.is-active {
-            background-color: var(--color-light);
-            color: var(--color-dark);
-        }
-
-        // State: Disabled
-        &:disabled {
-            background: none;
-            border: 1px solid var(--color-light-20);
-            cursor: not-allowed;
-        }
+    // State: Hover
+    &:hover {
+      background-color: var(--color-light-20);
     }
+
+    // State: Active
+    &.is-active {
+      background-color: var(--color-light);
+      color: var(--color-dark);
+    }
+
+    // State: Disabled
+    &:disabled {
+      background: none;
+      border: 1px solid var(--color-light-20);
+      cursor: not-allowed;
+    }
+  }
 </style>
--- src/components/organisms/Steps/MiddsSelection.svelte
+++ src/components/organisms/Steps/MiddsSelection.svelte
@@ -6,21 +6,30 @@
   import { type MiddsInputs, MusicalWork, Stakeholder } from "@allfeat/sdk";
   import type { IMidds } from "@allfeat/sdk";
 
-  enum Category {}
+  enum Category {
+    MusicalWork,
+    Track,
+  }
 
   class Entity {
     readonly name: string;
+    readonly category: Category;
 
-    constructor(name: string) {
+    constructor(name: string, category: Category) {
       this.name = name;
+      this.category = category;
     }
   }
 
   class ActiveEntity extends Entity {
     readonly instance: IMidds<MiddsInputs>;
 
-    constructor(name: string, instance: IMidds<MiddsInputs>) {
-      super(name);
+    constructor(
+      name: string,
+      category: Category,
+      instance: IMidds<MiddsInputs>,
+    ) {
+      super(name, category);
       this.instance = instance;
     }
   }
@@ -28,16 +37,18 @@
   class DisabledEntity extends Entity {
     readonly tag: string;
 
-    constructor(name: string, tag: string) {
-      super(name);
+    constructor(name: string, category: Category, tag: string) {
+      super(name, category);
       this.tag = tag;
     }
   }
 
   const Entities: Entity[] = [
-    new ActiveEntity("Stakeholder", new Stakeholder()),
-    new ActiveEntity("Musical Work", new MusicalWork()),
-    new DisabledEntity("Artist", "soon"),
+    new ActiveEntity("Stakeholder", Category.MusicalWork, new Stakeholder()),
+    new ActiveEntity("Musical Work", Category.MusicalWork, new MusicalWork()),
+    new DisabledEntity("Artist", Category.Track, "soon"),
+    new DisabledEntity("Release", Category.Track, "soon"),
+    new DisabledEntity("Track", Category.Track, "soon"),
   ];
 
   $effect(() => {
@@ -49,26 +60,80 @@
 <div class="inner">
   <HeadingIcon title="Choose a metadata entity" icon="entity" />
 
-  <div class="button-choices">
-    {#each Entities as entity}
-      {#if entity instanceof ActiveEntity}
-        <ButtonChoice
-          active={appState.selectedMiddsEntityName === entity.name}
-          onclick={() => (
-            (appState.selectedMiddsEntity = entity.instance),
-            (appState.selectedMiddsEntityName = entity.name)
-          )}
-        >
-          {entity.name}
-        </ButtonChoice>
-      {:else if entity instanceof DisabledEntity}
-        <ButtonChoice disabled>
-          {entity.name}
-          {#if entity.tag}
-            <Tag color="red">{entity.tag}</Tag>
-          {/if}
-        </ButtonChoice>
-      {/if}
-    {/each}
+  <div class="category musicalWork">
+    <p class="title">Musical Work related</p>
+    <div class="choices">
+      {#each Entities.filter((e) => e.category === Category.MusicalWork) as entity}
+        {#if entity instanceof ActiveEntity}
+          <ButtonChoice
+            active={appState.selectedMiddsEntityName === entity.name}
+            onclick={() => (
+              (appState.selectedMiddsEntity = entity.instance),
+              (appState.selectedMiddsEntityName = entity.name)
+            )}
+          >
+            {entity.name}
+          </ButtonChoice>
+        {:else if entity instanceof DisabledEntity}
+          <ButtonChoice disabled>
+            {entity.name}
+            {#if entity.tag}
+              <Tag color="red">{entity.tag}</Tag>
+            {/if}
+          </ButtonChoice>
+        {/if}
+      {/each}
+    </div>
   </div>
+
+  <div class="category track">
+    <p class="title">Track related</p>
+    <div class="choices">
+      {#each Entities.filter((e) => e.category === Category.Track) as entity}
+        {#if entity instanceof ActiveEntity}
+          <ButtonChoice
+            active={appState.selectedMiddsEntityName === entity.name}
+            onclick={() => (
+              (appState.selectedMiddsEntity = entity.instance),
+              (appState.selectedMiddsEntityName = entity.name)
+            )}
+          >
+            {entity.name}
+          </ButtonChoice>
+        {:else if entity instanceof DisabledEntity}
+          <ButtonChoice disabled>
+            {entity.name}
+            {#if entity.tag}
+              <Tag color="red">{entity.tag}</Tag>
+            {/if}
+          </ButtonChoice>
+        {/if}
+      {/each}
+    </div>
+  </div>
+
+  <div class="button-choices"></div>
 </div>
+
+<style lang="scss">
+  .category {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    padding: 16px;
+    width: 100%;
+    border: 1px solid var(--color-light-20);
+    border-radius: 4px;
+  }
+
+  .choices {
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    gap: 16px;
+  }
+
+  .title {
+    color: var(--color-light-50);
+  }
+</style>
